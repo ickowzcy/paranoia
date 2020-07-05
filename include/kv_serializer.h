6ce968cb83144e6f60bb877cb2e9011610c657d0
Ewerton Ickowzcy <ickowzcy.ewerton@gmail.com>
2020-07-05 12:22:56 +0900

    Improve textual representation of events

--- include/kv_serializer.h
+++ include/kv_serializer.h
@@ -5,17 +5,17 @@
 
 class KVSerializer {
  public:
-  [[nodiscard]] virtual std::string Encode(const std::map<std::string, std::string>& keyValuePairs) const = 0;
+  [[nodiscard]] virtual std::string Encode(const std::map<std::string, std::string>& kv_pairs) const = 0;
 };
 
 class KVTextSerializer : public KVSerializer {
  public:
-  [[nodiscard]] std::string Encode(const std::map<std::string, std::string>& keyValuePairs) const override;
+  [[nodiscard]] std::string Encode(const std::map<std::string, std::string>& kv_pairs) const override;
 };
 
 class KVJSONSerializer : public KVSerializer {
  public:
-  [[nodiscard]] std::string Encode(const std::map<std::string, std::string>& keyValuePairs) const override;
+  [[nodiscard]] std::string Encode(const std::map<std::string, std::string>& kv_pairs) const override;
 };
 
 #endif  // PARANOIA_KV_SERIALIZER_H
--- src/kv_serializer.cpp
+++ src/kv_serializer.cpp
@@ -1,14 +1,36 @@
 #include "kv_serializer.h"
 
+#include <algorithm>
 #include <iomanip>
 #include <nlohmann/json.hpp>
 #include <sstream>
 
-std::string KVTextSerializer::Encode(const std::map<std::string, std::string>& keyValuePairs) const {
+#include "process_event_keys.h"
+
+std::string toupper(std::string s) {
+  std::locale l;
+  std::transform(s.begin(), s.end(), s.begin(), [l](auto& c) { return std::toupper(c, l); });
+  return s;
+}
+
+std::string prettytimestamp(time_t t) {
   std::ostringstream oss;
+  oss << std::put_time(std::localtime(&t), "%F %T %Z");
+  return oss.str();
+};
+
+std::string KVTextSerializer::Encode(const std::map<std::string, std::string>& kv_pairs) const {
+  std::ostringstream oss;
+  oss << prettytimestamp(std::stoi(kv_pairs.at(TIMESTAMP_KEY)));
+  oss << " : ";
+  oss << toupper(kv_pairs.at(TYPE_KEY));
+  oss << " → ";
   oss << "[ ";
 
-  for (const auto& kv : keyValuePairs) {
+  for (const auto& kv : kv_pairs) {
+    if (TYPE_KEY == kv.first || TIMESTAMP_KEY == kv.first) {
+      continue;
+    }
     oss << kv.first;
     oss << ":";
     oss << std::quoted(kv.second);
@@ -20,8 +42,8 @@ std::string KVTextSerializer::Encode(const std::map<std::string, std::string>& k
   return oss.str();
 }
 
-std::string KVJSONSerializer::Encode(const std::map<std::string, std::string>& keyValuePairs) const {
-  nlohmann::json j(keyValuePairs);
+std::string KVJSONSerializer::Encode(const std::map<std::string, std::string>& kv_pairs) const {
+  nlohmann::json j(kv_pairs);
   std::ostringstream oss;
   oss << j;
   return oss.str();
