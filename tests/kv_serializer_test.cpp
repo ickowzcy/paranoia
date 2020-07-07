f05dd708ffd254d7f145454526b0b99467f68ec4
Ewerton Ickowzcy <ickowzcy.ewerton@gmail.com>
2020-07-07 20:05:43 +0900

    Refactor test

--- tests/kv_serializer_test.cpp
+++ tests/kv_serializer_test.cpp
@@ -1,5 +1,8 @@
 #include "kv_serializer.h"
 
+#include <iomanip>
+#include <sstream>
+
 #include "catch2/catch.hpp"
 #include "process_event_keys.h"
 
@@ -18,5 +21,23 @@ TEST_CASE("Key/Value pairs are serialized to json", "[kv_serializer]") {
   std::map<std::string, std::string> kv_test{
       {TYPE_KEY, TYPE_EXEC}, {TIMESTAMP_KEY, "0"}, {"foo", "42"}, {"bar", "(- -)"}};
 
-  REQUIRE("{\"bar\":\"(- -)\",\"foo\":\"42\",\"timestamp\":\"0\",\"type\":\"exec\"}" == serializer.Encode(kv_test));
+  std::ostringstream expected;
+  std::function<void(std::string, std::string, bool)> add_expected_term = [&expected](std::string&& k, std::string&& v,
+                                                                                      bool add_sep) {
+    expected << std::quoted(k);
+    expected << ":";
+    expected << std::quoted(v);
+    if (add_sep) {
+      expected << ",";
+    }
+  };
+
+  expected << "{";
+  add_expected_term("bar", "(- -)", true);
+  add_expected_term("foo", "42", true);
+  add_expected_term(TIMESTAMP_KEY, "0", true);
+  add_expected_term(TYPE_KEY, TYPE_EXEC, false);
+  expected << "}";
+
+  REQUIRE(expected.str() == serializer.Encode(kv_test));
 }
